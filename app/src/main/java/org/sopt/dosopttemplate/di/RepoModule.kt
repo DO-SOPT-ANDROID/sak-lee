61434bd7526b33db49390f88a1d7a1d2d882a2f4
lsakee <93514333+lsakee@users.noreply.github.com>
2023-10-19 22:45:56 +0900

    [FEAT] : repo di set #3

---
+++ app/src/main/java/org/sopt/dosopttemplate/di/RepoModule.kt
@@ -0,0 +1,18 @@
+package org.sopt.dosopttemplate.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.dosopttemplate.data.local.repo.AuthRepoImpl
+import org.sopt.dosopttemplate.domain.repo.AuthRepo
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepoModule {
+
+    @Singleton
+    @Binds
+    abstract fun providesAuthRepository(repoImpl: AuthRepoImpl): AuthRepo
+}
--- app/src/main/java/org/sopt/dosopttemplate/presentation/model/User.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/model/User.kt
@@ -2,6 +2,7 @@ package org.sopt.dosopttemplate.presentation.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import org.sopt.dosopttemplate.domain.entity.UserEntity
 
 @Parcelize
 data class User(
@@ -9,4 +10,6 @@ data class User(
     val pwd: String,
     val sojuCount: String,
     val nickname: String
-) : Parcelable
+) : Parcelable {
+    fun toUserEntity() = UserEntity(id, pwd, sojuCount, nickname)
+}
