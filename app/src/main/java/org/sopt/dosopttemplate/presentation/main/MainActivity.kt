8a453d223a20df1702a3ad7181fed9e30d7e2dd3
lsakee <93514333+lsakee@users.noreply.github.com>
2023-10-20 01:16:35 +0900

    [FEAT] : 2번 뒤로가기 #3

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
@@ -3,27 +3,39 @@ package org.sopt.dosopttemplate.presentation.main
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.databinding.ActivityMainBinding
-import org.sopt.dosopttemplate.presentation.model.User
-import org.sopt.dosopttemplate.presentation.signin.SignEvent
 import org.sopt.dosopttemplate.presentation.signin.SignInActivity
 import org.sopt.dosopttemplate.ui.context.repeatOnStarted
-import org.sopt.dosopttemplate.ui.context.snackBar
+import org.sopt.dosopttemplate.ui.context.toast
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
+    private var back: Long = 0
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - back >= 2000) {
+                back = System.currentTimeMillis()
+                toast("한번 더 종료")
+            } else {
+                finish()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+        this.onBackPressedDispatcher.addCallback(this, callback)
 
         initView()
         collectMainEvent()
