33d1e2af5988937f05842380d125edcab63d40bf
lsakee <93514333+lsakee@users.noreply.github.com>
2023-11-15 14:53:23 +0900

    [ADD] : NetworkModule #8

---
+++ app/src/main/java/org/sopt/dosopttemplate/di/NetworkModule.kt
@@ -0,0 +1,62 @@
+package org.sopt.dosopttemplate.di
+
+import android.util.Log
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONArray
+import org.json.JSONObject
+import org.sopt.dosopttemplate.BuildConfig
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor { message ->
+            when {
+                message.isJsonObject() ->
+                    Log.d("Retrofit2", JSONObject(message).toString(4))
+                message.isJsonArray() ->
+                    Log.d("Retrofit2", JSONArray(message).toString(4))
+                else -> {
+                    Log.d("Retrofit2", "CONNECTION INFO -> $message")
+                }
+            }
+        }
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    @Sopt
+    fun provideVelogRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(BuildConfig.AUTH_BASE_URL)
+        .client(okHttpClient)
+        .build()
+}
+
+
+fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
+fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
--- app/src/main/java/org/sopt/dosopttemplate/di/RepoModule.kt
+++ app/src/main/java/org/sopt/dosopttemplate/di/RepoModule.kt
@@ -11,7 +11,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepoModule {
-
     @Singleton
     @Binds
     abstract fun providesAuthRepository(repoImpl: AuthRepoImpl): AuthRepo
